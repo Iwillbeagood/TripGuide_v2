e22c06d28cd5335331ed45cc2cbe08ca22dddf22
jun <hky08067@naver.com>
2023-10-29 19:50:41 +0900

    data: 데이터베이스에 LocalTime을 저장하기 위한 컨버터

---
+++ core/database/src/main/java/com/jun/tripguide_v2_core/database/util/LocalTimeConverter.kt
@@ -0,0 +1,19 @@
+package com.jun.tripguide_v2_core.database.util
+
+import androidx.room.TypeConverter
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.LocalTime
+import java.time.ZoneOffset
+
+class LocalTimeConverter {
+    @TypeConverter
+    fun fromLocalTime(value: LocalTime?): Long? {
+        return value?.toSecondOfDay()?.toLong()
+    }
+
+    @TypeConverter
+    fun toLocalTime(value: Long?): LocalTime? {
+        return value?.let { LocalTime.ofSecondOfDay(it) }
+    }
+}
