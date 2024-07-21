2612ecc36a9309178481726d8836412da88d25c6
jhbae12346 <jhbae@logishm.com>
2024-07-22 07:57:17 +0900

    refactor: 정리 못한 api 제거

--- core/data/src/main/java/com/jun/tripguide_v2/core/data/repository/trainapi/TrainStationRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.jun.tripguide_v2.core.data.repository.trainapi
-
-import com.jun.tripguide_v2.core.model.TrainStation
-
-interface TrainStationRepository {
-
-    suspend fun getTrainStations(cityCode: Int): List<TrainStation>
-}
